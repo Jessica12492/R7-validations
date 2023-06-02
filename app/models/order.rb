8c6de232c66a7ed661dcebd14d5c74ecac4463b9
Jessica <jessica12492@gmail.com>
2023-06-02 08:31:41 -0500

    made  changes to models order.rb  for lesson 4.3

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   
  belongs_to :customer
- accepts_nested_attributes_for :customer
+ 
 
   validates :product_name, presence: true
   validates :product_count, presence: true
